6e8bf3627a751bc41463764ae3b89621b55500fa
acyanbird <acyanbird@gmail.com>
2024-11-15 19:19:39 +0800

    init

---
+++ fontsize.js
@@ -0,0 +1,129 @@
+var x = 93.75;
+var y = 150; 
+function larger() {
+	x *= 1.1;
+	document.body.style.fontSize = x + '%';
+	document.body.style.lineHeight = y + '%';
+	document.getElementById("DIVa").style.fontSize = '100%';
+	document.getElementById("DIVa").style.lineHeight = y + '%';	
+	document.getElementById("DIVb").style.fontSize = '100%';
+	document.getElementById("DIVb").style.lineHeight = y + '%';
+	
+	document.getElementById("DIVc").style.fontSize = '100%';
+	document.getElementById("DIVc").style.lineHeight = y + '%';	
+	document.getElementById("DIVd").style.fontSize = '100%';
+	document.getElementById("DIVd").style.lineHeight = y + '%';
+	document.getElementById("DIVe").style.fontSize = '100%';
+	document.getElementById("DIVe").style.lineHeight = y + '%';	
+	document.getElementById("DIVf").style.fontSize = '100%';
+	document.getElementById("DIVf").style.lineHeight = y + '%';
+	document.getElementById("DIVg").style.fontSize = '100%';
+	document.getElementById("DIVg").style.lineHeight = y + '%';	
+	document.getElementById("DIVh").style.fontSize = '100%';
+	document.getElementById("DIVh").style.lineHeight = y + '%';
+	document.getElementById("DIVi").style.fontSize = '100%';
+	document.getElementById("DIVi").style.lineHeight = y + '%';	
+	document.getElementById("DIVj").style.fontSize = '100%';
+	document.getElementById("DIVj").style.lineHeight = y + '%';
+	document.getElementById("DIVk").style.fontSize = '100%';
+	document.getElementById("DIVk").style.lineHeight = y + '%';	
+	document.getElementById("DIVl").style.fontSize = '100%';
+	document.getElementById("DIVl").style.lineHeight = y + '%';
+	document.getElementById("DIVm").style.fontSize = '100%';
+	document.getElementById("DIVm").style.lineHeight = y + '%';	
+	document.getElementById("DIVn").style.fontSize = '100%';
+	document.getElementById("DIVn").style.lineHeight = y + '%';
+	document.getElementById("DIVo").style.fontSize = '100%';
+	document.getElementById("DIVo").style.lineHeight = y + '%';	
+	document.getElementById("DIVp").style.fontSize = '100%';
+	document.getElementById("DIVp").style.lineHeight = y + '%';
+	document.getElementById("DIVq").style.fontSize = '100%';
+	document.getElementById("DIVq").style.lineHeight = y + '%';	
+	document.getElementById("DIVr").style.fontSize = '100%';
+	document.getElementById("DIVr").style.lineHeight = y + '%';
+	document.getElementById("DIVs").style.fontSize = '100%';
+	document.getElementById("DIVs").style.lineHeight = y + '%';	
+	document.getElementById("DIVt").style.fontSize = '100%';
+	document.getElementById("DIVt").style.lineHeight = y + '%';
+	document.getElementById("DIVu").style.fontSize = '100%';
+	document.getElementById("DIVu").style.lineHeight = y + '%';	
+	document.getElementById("DIVv").style.fontSize = '100%';
+	document.getElementById("DIVv").style.lineHeight = y + '%';
+	document.getElementById("DIVw").style.fontSize = '100%';
+	document.getElementById("DIVw").style.lineHeight = y + '%';	
+	document.getElementById("DIVx").style.fontSize = '100%';
+	document.getElementById("DIVx").style.lineHeight = y + '%';
+	document.getElementById("DIVy").style.fontSize = '100%';
+	document.getElementById("DIVy").style.lineHeight = y + '%';
+	document.getElementById("DIVz").style.fontSize = '100%';
+	document.getElementById("DIVz").style.lineHeight = y + '%';
+		
+	}
+
+function smaller() {
+	x /= 1.1;
+	document.body.style.fontSize = x + '%';
+	document.body.style.lineHeight = y + '%';
+	document.getElementById("DIVa").style.fontSize = '100%';
+	document.getElementById("DIVa").style.lineHeight = y + '%';	
+	document.getElementById("DIVb").style.fontSize = '100%';
+	document.getElementById("DIVb").style.lineHeight = y + '%';
+	
+	document.getElementById("DIVc").style.fontSize = '100%';
+	document.getElementById("DIVc").style.lineHeight = y + '%';	
+	document.getElementById("DIVd").style.fontSize = '100%';
+	document.getElementById("DIVd").style.lineHeight = y + '%';
+	document.getElementById("DIVe").style.fontSize = '100%';
+	document.getElementById("DIVe").style.lineHeight = y + '%';	
+	document.getElementById("DIVf").style.fontSize = '100%';
+	document.getElementById("DIVf").style.lineHeight = y + '%';
+	document.getElementById("DIVg").style.fontSize = '100%';
+	document.getElementById("DIVg").style.lineHeight = y + '%';	
+	document.getElementById("DIVh").style.fontSize = '100%';
+	document.getElementById("DIVh").style.lineHeight = y + '%';
+	document.getElementById("DIVi").style.fontSize = '100%';
+	document.getElementById("DIVi").style.lineHeight = y + '%';	
+	document.getElementById("DIVj").style.fontSize = '100%';
+	document.getElementById("DIVj").style.lineHeight = y + '%';
+	document.getElementById("DIVk").style.fontSize = '100%';
+	document.getElementById("DIVk").style.lineHeight = y + '%';	
+	document.getElementById("DIVl").style.fontSize = '100%';
+	document.getElementById("DIVl").style.lineHeight = y + '%';
+	document.getElementById("DIVm").style.fontSize = '100%';
+	document.getElementById("DIVm").style.lineHeight = y + '%';	
+	document.getElementById("DIVn").style.fontSize = '100%';
+	document.getElementById("DIVn").style.lineHeight = y + '%';
+	document.getElementById("DIVo").style.fontSize = '100%';
+	document.getElementById("DIVo").style.lineHeight = y + '%';	
+	document.getElementById("DIVp").style.fontSize = '100%';
+	document.getElementById("DIVp").style.lineHeight = y + '%';
+	document.getElementById("DIVq").style.fontSize = '100%';
+	document.getElementById("DIVq").style.lineHeight = y + '%';	
+	document.getElementById("DIVr").style.fontSize = '100%';
+	document.getElementById("DIVr").style.lineHeight = y + '%';
+	document.getElementById("DIVs").style.fontSize = '100%';
+	document.getElementById("DIVs").style.lineHeight = y + '%';	
+	document.getElementById("DIVt").style.fontSize = '100%';
+	document.getElementById("DIVt").style.lineHeight = y + '%';
+	document.getElementById("DIVu").style.fontSize = '100%';
+	document.getElementById("DIVu").style.lineHeight = y + '%';	
+	document.getElementById("DIVv").style.fontSize = '100%';
+	document.getElementById("DIVv").style.lineHeight = y + '%';
+	document.getElementById("DIVw").style.fontSize = '100%';
+	document.getElementById("DIVw").style.lineHeight = y + '%';	
+	document.getElementById("DIVx").style.fontSize = '100%';
+	document.getElementById("DIVx").style.lineHeight = y + '%';
+	document.getElementById("DIVy").style.fontSize = '100%';
+	document.getElementById("DIVy").style.lineHeight = y + '%';
+	document.getElementById("DIVz").style.fontSize = '100%';
+	document.getElementById("DIVz").style.lineHeight = y + '%';
+	
+	}
+
+
+
+
+
+function large() { document.body.style.fontSize = "120%"; document.body.style.lineHeight ="120%"; }
+function middle() { document.body.style.fontSize = "93.75%"; document.body.style.lineHeight ="150%";}
+function smal() { document.body.style.fontSize = "90%"; document.body.style.lineHeight ="90%"; }
